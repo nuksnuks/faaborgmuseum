a50af2b9bee3b8848b5c0b570c9eda5c0f19aab7
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2022-11-18 11:03:42 +0100

    database

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'faaborgmuseum' );
+define( 'DB_NAME', 'nuksnuks_dkdev_faaborgmuseum' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'nuksnuks_dkdev_faaborgmuseum@10.27.48.48' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'mmoe21team10' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'https://dbadmin.one.com/' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
