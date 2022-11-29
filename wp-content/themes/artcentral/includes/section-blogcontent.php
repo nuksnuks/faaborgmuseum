94427b8caae0ae97e48a68d89120764bd959267b
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2022-11-29 10:47:19 +0100

    body stylet

--- wp-content/themes/artcentral/includes/section-blogcontent.php
+++ wp-content/themes/artcentral/includes/section-blogcontent.php
@@ -1,6 +1,12 @@
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-
+  <?php echo get_the_date();?>
   <?php the_content(); ?> <br>
+
+  <?php
+    $fname = get_the_author_meta('first_name');
+    $lname = get_the_author_meta('last_name');
+  ?>
+  <p>Skrevet af: <?php $fname;?> <?php $lname;?></p>
   <?php the_author(); ?>
 
 <?php endwhile; else: endif;?>
--- wp-content/themes/artcentral/includes/section-content.php
+++ wp-content/themes/artcentral/includes/section-content.php
@@ -1,6 +1,4 @@
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
 <?php the_content(); ?> <br>
-<?php
-  
-?>
+
 <?php endwhile; else: endif;?>
