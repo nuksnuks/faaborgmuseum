acfbafa0d7dd966f118e81ca994125521e71b2ff
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2022-12-06 11:36:59 +0100

    this is gonna mess it all up my guy

--- wp-content/themes/artcentral/searchform.php
+++ wp-content/themes/artcentral/searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get">
+<form action="/" method="get" class="searchbar">
   <label for="search">Søg</label>
   <input type="text" name="s" id="searchbar" value="<?php the_search_query();?>" required>
   <button type="submit">Søg</button>
