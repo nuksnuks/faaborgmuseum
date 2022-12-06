f93a5a28a5a64e61186b81d149614e31928422c3
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2022-12-06 14:49:07 +0100

    kmskdmfaæsdf

--- wp-content/themes/artcentral/js/main.js
+++ wp-content/themes/artcentral/js/main.js
@@ -3,7 +3,28 @@ const searchbar = document.getElementById('searchbar');
 const button = document.getElementById('searchbutton');
 
 function expand(){
-  alert("what dafuq");
-}
+      if(searchbar.style.display == 'none'){
+      searchbar.style.display = 'block';
+      button.style.display = 'block';
+    }
+    else{
+      searchbar.style.display = 'none';
+      button.style.display = 'none';
+    }
+  };
 
 label.addEventListener('click', expand);
+
+function changecolor(){
+  menu = document.getElementById('menustylingsanker')
+if(scrollY>0){
+  menu.style.background = '';
+  menu.style.background = 'linear-gradient(180deg, rgba(110,68,69,1) 0%, rgba(255,255,255,0) 100%)';
+}
+else{
+menu.style.background = '';
+}
+
+};
+
+window.addEventListener('scroll', changecolor);
